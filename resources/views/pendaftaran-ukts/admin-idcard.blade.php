d3597532011bb1d18e7e58e2e783d3222c4ab015
ancadev05 <hamzah.dev.061223@gmail.com>
2025-04-17 13:43:34 +0800

    perbaikan id card

--- resources/views/pendaftaran-ukts/admin-idcard.blade.php
+++ resources/views/pendaftaran-ukts/admin-idcard.blade.php
@@ -9,14 +9,14 @@
         <a href="{{ url('/cetak-idcard') }}" class="btn btn-sm btn-primary mb-3" target="_blank">Cetak Idcard</a>
         <div class="d-flex flex-wrap">
             @foreach ($idcard as $item)
-                <div style="width: 10.2cm; height: 6.5cm;" class="me-2">
-                    <table class="table">
+                <div style="width: 9cm; height: 6.5cm; font-size: 14px;" class="me-3 mb-3 px-2 border d-flex justify-content-center align-items-center">
+                    <table class="w-100 me-3">
                         <tr>
                             <td>No. Peserta</td>
                             <td>: {{ $item->id }}</td>
                             <td rowspan="5">
-                                <img src="{{ url('storage/foto-pesilat/' . $item->pesilat->foto_pesilat) }}" alt=""
-                                    width="3cm">
+                                <img src="{{ url('storage/foto-pesilat/' . $item->pesilat->foto_pesilat) }}" alt="" style="width: 2cm">
+                                {{-- <img src="{{ url('foto.jpg') }}" alt="" style="width: 3cm"> --}}
                             </td>
                         </tr>
                         <tr>
